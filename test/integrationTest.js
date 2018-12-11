4a4623f22242c2dcc4d46e4836a00fd7322d0d57
qoire <mr.sun.yao@gmail.com>
2018-12-11 11:23:13 -0500

    AionTransactionIntegrationTest#testCase1

---
+++ test/integrationTest.js
@@ -0,0 +1,7 @@
+const assert = require('chai').assert;
+const RLP = require('../index.js');
+const BN = require('bn.js');
+const AionLong = RLP.AionLong;
+
+describe("Describes corner cases that pop up", () => {
+});
